4c3dc17e78b56302457a5fc2320faf5badda0b04
Lumko Majozi <lumajjhb025@student.wethinkcode.co.za>
2026-08-17 21:00:13 +0200

    Added Expense class

---
+++ lib/expense.dart
@@ -0,0 +1,11 @@
+class Expense {
+  String description;
+  double amount;
+  String category;
+
+  Expense(String description, double amount, String category) {
+    this.description = description;
+    this.amount = amount;
+    this.category = category;
+  }
+}
