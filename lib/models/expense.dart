4dd440db9f87a0c10587fe40ed7b23ab2027087b
Lumko Majozi <lumajjhb025@student.wethinkcode.co.za>
2026-09-21 17:03:11 +0200

    feat: add JSON serialization for expense storage

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -5,6 +5,22 @@ class Expense {
 
   Expense(this.description, this.amount, this.category);
 
+  Map<String, dynamic> toJson() {
+    return {
+      'description': description,
+      'amount': amount,
+      'category': category,
+    };
+  }
+
+  factory Expense.fromJson(Map<String, dynamic> json) {
+    return Expense(
+      json['description'] as String,
+      (json['amount'] as num).toDouble(),
+      json['category'] as String,
+    );
+  }
+
   @override
   String toString() {
     return '$description - R$amount - $category';
